a2b4239084cb7c4f09ea1991570ddc4fc6468364
choiruzain-latrobe <127911801+choiruzain-latrobe@users.noreply.github.com>
2024-02-12 14:57:51 +1100

    init1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './Card.jsx'
+
+function App() {
+  return(
+      <> // fragment to put all five react components all together
+        <Card /> 
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </>
+  )
+}
+export default App
